108f6fcf5ea428d4a6b4a6aa9321ac5391bd0c42
longtn <longtran18320@gmail.com>
2023-01-08 09:16:17 +0700

    add braintree

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Import packages
 const express = require('express');
 const home = require('./routes/home');
-
+const brainTreeRouter = require('./routes/braintree');
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
 app.use('/home', home);
+app.use('/api', brainTreeRouter);
 
 // connection
 const port = process.env.PORT || 9001;
